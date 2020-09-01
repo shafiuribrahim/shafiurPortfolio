c264c0b1197523ce62e63aba912f63d81830c427
Shafiur Ibrahim <shafiuribrahim@gmail.com>
2020-09-01 16:46:54 -0400

    testing

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,7 +2,7 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Shafiur Ibrahim', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
